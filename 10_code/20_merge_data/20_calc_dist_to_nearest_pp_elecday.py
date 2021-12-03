eaeafccda4c8344e613fcaa602c7c3bded600c7a
jgy4 <69827274+jgy4@users.noreply.github.com>
2021-12-02 21:20:06 -0500

    Add final tables & merge updates

--- 10_code/20_merge_data/20_calc_dist_to_nearest_pp_elecday.py
+++ 10_code/20_merge_data/20_calc_dist_to_nearest_pp_elecday.py
@@ -23,11 +23,37 @@ wkt = """PROJCS["USA_Contiguous_Equidistant_Conic",
 campuses = gpd.read_file(
     "../../20_intermediate_files/20_campus_polygons_w_demographics.geojson"
 )
+#Drop excess columns
+campuses = campuses.drop(columns=['SLSV Coalition', 'ALL IN', 'AGF', 'Ask Every Student', 'Campus Vote Project','Voter Friendly Campus', 'StudentPIRGs', 'Aliento Education Fund', 'Alliance for Youth Organizing', 'American Democracy Project', 
+                   " Arizona Students' Association", 'Baltimore Collegetown Network\t\t', 'Black Girls Vote', 
+                   'Boston Votes Coalition', 'California Campus Compact ', 'CALPIRG Students', 'CEEP', 'Civic Nebraska', 
+                   'Common Cause', 'Count US IN', 'Creative Campus Voting Project', 'Day on Democracy', 'Democracy Works', 
+                   'Engage Miami', 'Every Vote Counts', 'Forward Montana', 'Georgia Shift', 'Hillel International', 
+                   'IGNITE National', 'IA & MN Campus Compact', 'LeadMN', 'Loud Light', 'Maine Students Vote', 'MARYPIRG', 
+                   'MI Familia Vota', 'Minnesota Youth Collective', 'Mississippi Votes', 'NASPAA', 'New Era Colorado', 
+                   'NYPIRG', 'NCAAT', 'NCPIRG', 'NC Campus Compact', 'Ohio Campus Compact', 'PIRGIM Campus Action', 
+                   'Project Pericles', 'TurnUp Activism', 'Up to Us/ Net Impact', 'VoteRiders', 'Washington Student Association', 'Xceleader (Vote HBCU)', 'NVEW 2020', 'NVRD\n2017', 'NVRD 2018', 'NVRD 2020'])
 
 campuses = campuses.to_crs(wkt)
 
 campuses = campuses.copy()
-year = 2018
+
+#Get set of states common to all polling place data
+p = gpd.read_file(
+        f"../../20_intermediate_files/10_polling_places/2020_BallotReady_Early.geojson"
+    )
+state_set = set(p['state'].unique())
+
+#Loop through all CPI Years for common states
+for year in [2012, 2016, 2018, 2020]:
+    p = gpd.read_file(
+        f"../../20_intermediate_files/10_polling_places/{year}_elecday_cpi.geojson"
+    )
+    state_set = set(state_set & set(p['state'].unique()))
+    
+#Limit campuses to those within our common states
+campuses = campuses[campuses['STATE'].isin(state_set)]
+temp_list = []
 
 # Load all polling data, get nearest
 for year in [2012, 2016, 2018, 2020]:
@@ -46,31 +72,23 @@ for year in [2012, 2016, 2018, 2020]:
     p = p.rename({"state": f"state_{year}"}, axis="columns")
     p = p.rename({"Latitude": f"Latitude_{year}"}, axis="columns")
     p = p.rename({"Longitude": f"Longitude_{year}"}, axis="columns")
-
-    # Get distance to nearest
-    # TO DO: Make within-state!
-    # Note that when multiple pps are same distance, get
-    # dup records, and results in some weird N increases.
-    # need to investigate more.
-
-    # Mostly coming from 2018 merge?
-
-    print("pre: ")
-    print(len(campuses))
-    campuses = campuses.sjoin_nearest(p, distance_col=f"distances_{year}", how="left")
-    print("post: ")
-    print(len(campuses))
     
-    campuses = campuses.drop("index_right", axis="columns")
-    campuses = campuses.drop_duplicates()
+    # Get distance to nearest by looping through states, then concat
+    temp = pd.DataFrame()
+    for state in state_set:
+        temp1 = pd.DataFrame()
+        temp1 = campuses[campuses['STATE'] == state].sjoin_nearest(p[p[f"state_{year}"] == state], distance_col=f"distances_{year}", how="left")
+        #Remove multiple equidistant polling places
+        temp1 = temp1.drop_duplicates(subset=['UNIQUEID'], keep='last')
+        temp = pd.concat([temp, temp1])
+    temp_list.append(temp)
 
-    # Drop if nearest out-of-state
-    campuses.loc[
-        (campuses["STATE"] != campuses[f"state_{year}"])
-        & campuses[f"distances_{year}"].notnull(),
-        f"distances_{year}",
-    ] = np.nan
+#Bring all polling years together in one dataframe
+for i in range(1,len(temp_list)):
+    temp_list[0] = temp_list[0].merge(temp_list[i].iloc[:,[0,-3,-2,-1]],on='UNIQUEID',how='inner')
 
+#Set final df
+final_df = temp_list[0]
 
 #Add Ballot Ready Polling Data
 p = gpd.read_file(
@@ -85,29 +103,18 @@ year = '2020_early'
 p = p.rename({"state": f"state_{year}"}, axis="columns")
 p = p.rename({"Latitude": f"Latitude_{year}"}, axis="columns")
 p = p.rename({"Longitude": f"Longitude_{year}"}, axis="columns")
-campuses = campuses.sjoin_nearest(p, distance_col=f"distances_{year}", how="left")
-campuses = campuses.drop("index_right", axis="columns")
-campuses = campuses.drop_duplicates()
+temp = pd.DataFrame()
 
-# Drop if nearest out-of-state
-campuses.loc[
-    (campuses["STATE"] != campuses[f"state_{year}"])
-    & campuses[f"distances_{year}"].notnull(),
-    f"distances_{year}",
-    ] = np.nan
+#Loop through states and merge with final dataframe
+for state in state_set:
+    temp1 = pd.DataFrame()
+    temp1 = campuses[campuses['STATE'] == state].sjoin_nearest(p[p[f"state_{year}"] == state], distance_col=f"distances_{year}", how="left")
+    #Remove multiple equidistant polling places
+    temp1 = temp1.drop_duplicates(subset=['UNIQUEID'], keep='last')
+    temp = pd.concat([temp, temp1])
+final_df = final_df.merge(temp.iloc[:,[0,-3,-2,-1]],on='UNIQUEID',how='inner')
 
-#Drop excess columns
-campuses = campuses.drop(columns=['SLSV Coalition', 'ALL IN', 'AGF', 'Ask Every Student', 'Campus Vote Project','Voter Friendly Campus', 'StudentPIRGs', 'Aliento Education Fund', 'Alliance for Youth Organizing', 'American Democracy Project', 
-                   " Arizona Students' Association", 'Baltimore Collegetown Network\t\t', 'Black Girls Vote', 
-                   'Boston Votes Coalition', 'California Campus Compact ', 'CALPIRG Students', 'CEEP', 'Civic Nebraska', 
-                   'Common Cause', 'Count US IN', 'Creative Campus Voting Project', 'Day on Democracy', 'Democracy Works', 
-                   'Engage Miami', 'Every Vote Counts', 'Forward Montana', 'Georgia Shift', 'Hillel International', 
-                   'IGNITE National', 'IA & MN Campus Compact', 'LeadMN', 'Loud Light', 'Maine Students Vote', 'MARYPIRG', 
-                   'MI Familia Vota', 'Minnesota Youth Collective', 'Mississippi Votes', 'NASPAA', 'New Era Colorado', 
-                   'NYPIRG', 'NCAAT', 'NCPIRG', 'NC Campus Compact', 'Ohio Campus Compact', 'PIRGIM Campus Action', 
-                   'Project Pericles', 'TurnUp Activism', 'Up to Us/ Net Impact', 'VoteRiders', 'Washington Student Association', 'Xceleader (Vote HBCU)', 'NVEW 2020', 'NVRD\n2017', 'NVRD 2018', 'NVRD 2020'])
-
-campuses.to_file(
+#Save to file
+final_df.to_file(
     "../../20_intermediate_files/30_campuses_w_dist_to_nearest_pp.geojson",
-    driver="GeoJSON",
-)
+    driver="GeoJSON")
