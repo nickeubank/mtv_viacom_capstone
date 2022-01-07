8d73746c42d387745963669f90cb7299c17dd7ca
Pranav Manjunath <pranavmanjunath@Pranavs-MBP.lan>
2022-01-07 11:48:12 -0500

    stats

--- 10_code/30_analysis/112_distance_api.py
+++ 10_code/30_analysis/112_distance_api.py
@@ -71,8 +71,45 @@ def main_distance(org_y,org_x,dest_y,dest_x,data,i):
             #    col3='fare_in_dollars'
             #    data.loc[i,col3]=fare
 
-            col1='2020_distance_by_'+mode+' (miles)'
-            col2='2020_duration_by_'+mode+' (minutes)'
+            col1='early_2020_distance_by_'+mode+' (miles)'
+            col2='early_2020_duration_by_'+mode+' (minutes)'
+            #print("Distance: ",distance)
+            #print("Duration: ", duration)
+            data.loc[i,col1]=distance
+            data.loc[i,col2]=duration
+
+
+
+
+def main_distance1(org_y,org_x,dest_y,dest_x,data,i):
+    api_key="AIzaSyAqWiye83cMRhMGBq2M5fesfUuO1NT4lbw"
+    #adding the three different type of mode
+    for mode in ['walking','driving','transit']:
+
+        #extract detail from API
+        url =  "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+org_y+"%2C"+org_x+"&destinations="+dest_y+"%2C"+dest_x+"&mode="+mode+"&key="+api_key
+        payload={}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        #print(response.json())
+
+        if response.json()['rows'][0]['elements'][0]['status'] == "ZERO_RESULTS":
+        #if response.json()['status']== "ZERO_RESULTS":
+            break
+        else:
+            #Calculate distance and duration
+            distance=extract_distance(response)
+            duration=extract_duration(response)
+
+            #if mode =='transit' and "fare" in response.json()['rows'][0]['elements'][0]:
+            #    fare=extract_fare(response)
+            #    col3='fare_in_dollars'
+            #    data.loc[i,col3]=fare
+
+            col1='Starbucks_distance_by_'+mode+' (miles)'
+            col2='Starbucks_duration_by_'+mode+' (minutes)'
             #print("Distance: ",distance)
             #print("Duration: ", duration)
             data.loc[i,col1]=distance
@@ -84,20 +121,33 @@ def load_data():
     #input the data
     input_filename = "30_campuses_w_dist_to_nearest_pp"
     print(input_filename)
-    data=gpd.read_file("../../20_intermediate_files/"+input_filename+".geojson")
+    #data=gpd.read_file("../../20_intermediate_files/"+input_filename+".geojson")
 
+    data=pd.read_csv("../../20_intermediate_files/"+input_filename+".csv")
     #data = data[data['Longitude_left'].notna()]
     data=data.drop_duplicates(subset=['School Name'])
+    count=0
     for i,r in data.iterrows():
+        count+=1
+        print(count)
         #extracting the polling location and passing the lat long for college and polling into main_distance
         #polling_location=list(data.loc[i,'nearest_polling_geometry'][7:-1].split())
     
         x_pol,y_pol=data.loc[i,'Longitude_2020_early'],data.loc[i,'Latitude_2020_early']
-        x_col,y_col=data.loc[i,'centroid_long'],data.loc[i,'centroid_lat']
+        #x_target,y_target=data.loc[i,'Longitude_target'],data.loc[i,'Latitude_target']
 
+        #x_starbucks,y_starbucks=data.loc[i,'Longitude_starbucks'],data.loc[i,'Latitude_starbucks']
+        x_col,y_col=data.loc[i,'centroid_long'],data.loc[i,'centroid_lat']
+  
         main_distance(str(y_col),str(x_col),str(y_pol),str(x_pol),data,i)
 
-    _=data.to_file("../../20_intermediate_files/"+input_filename+".geojson", driver="GeoJSON")
+        #main_distance1(str(y_col),str(x_col),str(y_starbucks),str(x_starbucks),data,i)
+
+    
+    _=data.to_csv("../../20_intermediate_files/"+input_filename+".csv")
+
+
+    #_=data.to_file("../../20_intermediate_files/"+input_filename+".geojson", driver="GeoJSON")
     
     #geo_df.to_file(
     #"../../20_intermediate_files/10_HIFLD_campus_polygons.geojson", driver="GeoJSON")
